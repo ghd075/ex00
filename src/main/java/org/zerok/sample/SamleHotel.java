92c3d9c88b7fd15c7280e18e1eee8e1ba2649455
ghd075 <rlfls54@gmail.com>
2019-12-11 18:39:41 +0900

    @NonNull과 @RequiredArgsConstructor 사용법에 관한 실습!!

--- src/main/java/org/zerok/sample/SamleHotel.java
+++ src/main/java/org/zerok/sample/SamleHotel.java
@@ -2,16 +2,18 @@ package org.zerok.sample;
 
 import org.springframework.stereotype.Component;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 @Component
 @ToString
 @Getter
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class SamleHotel {
 
+	@NonNull
 	private Chef chef;
 	
 	
