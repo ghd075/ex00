cf76624043b8d8a07498a63b5e66a5777a3fccc5
ghd075 <rlfls54@gmail.com>
2019-12-11 18:37:57 +0900

    생성자 자동 주입 실습!!

--- src/main/java/org/zerok/sample/SamleHotel.java
+++ src/main/java/org/zerok/sample/SamleHotel.java
@@ -2,17 +2,17 @@ package org.zerok.sample;
 
 import org.springframework.stereotype.Component;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 
 @Component
 @ToString
 @Getter
+@AllArgsConstructor
 public class SamleHotel {
 
 	private Chef chef;
 	
-	public SamleHotel(Chef chef) {
-		this.chef = chef;
-	}
+	
 }
